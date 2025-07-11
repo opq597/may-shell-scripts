2cb873a6c9eb91b4c994e233baf259a9e674d868
TEJAS2211 <131863535+TEJAS2211@users.noreply.github.com>
2025-07-12 01:12:14 +0530

    DAY6 ASSIGNMENT

--- day6/TEJASWI/Assignment2_disk_usage.sh
+++ day6/TEJASWI/Assignment2_disk_usage.sh
@@ -3,6 +3,7 @@ a=$(df -h |sed 's/%//g' |awk -F " " 'NR>1 && $5>80 {print $5}'|head -n 1)
 if [ -n "$a" ]
 then
 	echo "WARNING : DISK USAGE IS MORE THAN 80 PERCENT"
+        mail -s "DISK USAGE --WARNING MORE THAN 80 PERCENT " ubuntu
 else
 	echo " DISK USAGE --NORMAL "
 fi
