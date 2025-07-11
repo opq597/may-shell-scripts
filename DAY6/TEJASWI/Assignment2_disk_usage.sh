8caa1a00833f5996bc5fe0b722f3c697e8433a00
TEJAS2211 <131863535+TEJAS2211@users.noreply.github.com>
2025-07-12 00:35:46 +0530

    day6 assignment

--- DAY6/TEJASWI/Assignment2_disk_usage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-a=$(df -h |sed 's/%//g' |awk -F " " 'NR>1 && $5>80 {print $5}'|head -n 1)
-if [ -n "$a" ]
-then
-	echo "WARNING : DISK USAGE IS MORE THAN 80 PERCENT"
-else
-	echo " DISK USAGE --NORMAL "
-fi
