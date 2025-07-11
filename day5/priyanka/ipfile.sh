adcb7380989173bf909d5bd2ef50ba40e39479ae
Priyanka-smg <gowdapriyanka007@gmail.com>
2025-07-11 17:07:37 +0000

    removing

--- day5/priyanka/ipfile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-name="priyanka"
-age=25
-job="QA"
--- day5/priyanka/passfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ./ipfile.sh
-#while read line
-#do
- #   eval "$line"
-#done
-echo "my name is $name"
-echo "my job is $job and i am $age years of age"
